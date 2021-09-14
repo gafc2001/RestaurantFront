38cdf4233cf981690e403a2550172b5dc3868410
david alexander <davidalexd1234@gmail.com>
2021-09-14 09:47:18 -0500

    probando en notify

--- src/components/Notify.js
+++ src/components/Notify.js
@@ -1,12 +1,38 @@
 import React ,{ Component } from 'react';
-class Notify extends Component{
-    render(){
+import { FormGroup } from 'react-bootstrap';
+import {Button,Modal,ModalHeader,ModalBody,ModalFooter,Input, Label} from 'reactstrap';
+import { useState} from "react";
+export default function Notify() {
+    
+    const [state, setstate] = useState(false)
+   const abrirModal=()=>{
+        setstate(!state);
+    }
         return(
-            <div>
-                <h4>este es las Notificaciones </h4>
+            <>
+            <div className="principal">
+                <div className="secundario">
+                    <Button color="success" onClick={abrirModal}>MOdal</Button>
+                </div>
+                <Modal isOpen={state}>
+                    <ModalHeader>inicia</ModalHeader>
+                    <ModalBody>
+                        <FormGroup>
+                            <Label for="usuario">susuario</Label>
+                            <Input type="text" id="usuario">
+                            </Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="password">contssssa</Label>
+                            <Input type="text"></Input>
+                        </FormGroup>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button>inicar</Button>
+
+                    </ModalFooter>
+                </Modal>
             </div>
-            
+            </>
         );
     }
-}
-export default Notify;
