631be232582622d130f0698ede664c08c90b3a2a
david alexander <davidalexd1234@gmail.com>
2021-10-01 16:33:41 -0500

    homeview arreglando

--- src/components/home/Product.js
+++ src/components/home/Product.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../home/home.css";
 import "../../assets/css/style.css";
 const Product = ({ data, addToCart}) => {
-  let { idProduct, nameProduct, priceProduct, description } = data;
+  let { idProduct, nameProduct, priceProduct,category} = data;
   return (
     <div className="product-card">
       <div className="card-image center">
@@ -14,7 +14,7 @@ const Product = ({ data, addToCart}) => {
       <div className="card-info">
         <p className="product-name">{nameProduct}</p>
         <p className="product-price">${priceProduct}.00</p>
-        <p className="product-available">{description}</p>
+        <p className="product-available">{category.nameCategory}</p>
       </div>
       <div className="btn-container">
         <button className="btn btn-primary " onClick={() => (addToCart(idProduct)) }>
