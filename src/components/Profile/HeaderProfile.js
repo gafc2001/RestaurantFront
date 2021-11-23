60ef37e7b4eec51f4b379a5f88e7fabb1c2cd3d6
davidalexd <davidalexd1234@gmail.com>
2021-11-23 17:38:20 -0500

    cambios en proceso de perfil final

--- src/components/Profile/HeaderProfile.js
+++ src/components/Profile/HeaderProfile.js
@@ -6,12 +6,11 @@ import PictureForm from "./PictureForm";
 
 Modal.setAppElement("#root");
 
-
 const idcli = sessionStorage.getItem("id");
 const HeaderProfile = () => {
   const [imgPreview, setImgPreview] = useState(null);
   const [error, setError] = useState(false);
-  const [img, setImg] = useState(null)
+  const [img, setImg] = useState(null);
 
   const [modalIsOpen, setIsOpen] = useState(false);
 
@@ -30,7 +29,7 @@ const HeaderProfile = () => {
     },
     overlay: {
       position: "fixed",
-      zIndex : 1000,
+      zIndex: 1000,
       top: 0,
       left: 0,
       right: 0,
@@ -41,7 +40,7 @@ const HeaderProfile = () => {
 
   const HandleImagefile = (e) => {
     let selected = e.target.files[0];
-    setImg(selected)
+    setImg(selected);
     const ALLOWED_TYPES = ["image/png", "image/jpeg", "image/jpg"];
     if (selected && ALLOWED_TYPES.includes(selected.type)) {
       let reader = new FileReader();
@@ -51,10 +50,12 @@ const HeaderProfile = () => {
       reader.readAsDataURL(selected);
     } else {
       setError(true);
+      setTimeout(() => setError(false), 3000);
+      setIsOpen(false);
     }
     //setForm({ ...form, profilePicture: selectedFile });
   };
-  const ImageSubmit = ()=>{
+  const ImageSubmit = () => {
     if (img) {
       //guardando imagenes
       const data = new FormData();
@@ -67,48 +68,46 @@ const HeaderProfile = () => {
         .then((resp) => resp)
         .then((resp) => {
           console.log(resp);
-          setImgPreview(null)
-        })
+          setImgPreview(null);
+          setIsOpen(false);
+        });
       return;
-    } 
-
-  }
+    }
+  };
   return (
-    
     <header className="profile-header">
-      {imgPreview&&
-      <Modal
-          isOpen={modalIsOpen}
-          style={customStyles}
-          contentLabel="Picture"
-      >
-          <PictureForm imgPreview={imgPreview} ImageSubmit={ImageSubmit} setImgPreview={setImgPreview} HandleImagefile={HandleImagefile}/>
-      </Modal>}
-      <div className="profile-image-container">
-        {!imgPreview && (
-          <>
-
-            <img src={`${URL.USERS_DB}/${idcli}/image`} />
-            <span className="edit center" onClick={()=>setIsOpen(true)}>
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" clipRule="evenodd" d="M3.96867 13.9157L13.6592 4.41662C14.906 3.19446 16.9283 3.19446 18.175 4.41662L19.4694 5.6854C20.7162 6.90755 20.7162 8.88985 19.4694 10.112L9.74061 19.6486C9.1843 20.1939 8.43007 20.4999 7.64282 20.4999H3.65854C3.28841 20.4999 2.99098 20.201 3.00021 19.8383L3.10043 15.8975C3.12036 15.1526 3.43127 14.4425 3.96867 13.9157ZM18.5381 6.59831L17.2437 5.32953C16.5113 4.61156 15.323 4.61156 14.5905 5.32953L13.8382 6.06697L17.7862 9.93612L18.5381 9.19909C19.2705 8.48112 19.2705 7.31628 18.5381 6.59831ZM4.89998 14.8287L12.9069 6.97989L16.8549 10.849L8.8093 18.7357L8.70228 18.8317C8.4067 19.0745 8.03222 19.2088 7.64282 19.2088L4.33345 19.2084L4.41707 15.9305C4.42814 15.5169 4.60126 15.1215 4.89998 14.8287ZM21 19.8545C21 19.498 20.7052 19.2089 20.3415 19.2089H13.471L13.3816 19.2148C13.0602 19.2576 12.8125 19.5277 12.8125 19.8545C12.8125 20.211 13.1073 20.5 13.471 20.5H20.3415L20.4308 20.4941C20.7523 20.4514 21 20.1813 21 19.8545Z" fill="#200E32"/>
-              </svg>
-            </span>
-          </>
-        )}
-        
-        {imgPreview&&<>
-        
-          <button className="btn btn-secondary" onClick={ImageSubmit}>Aceptar image</button>
-        <button className="btn btn-secondary" onClick={()=>setImgPreview(null)}>remover image</button>
-        <img src={imgPreview} /> 
-        </>}
-        
-   
-      </div>
-      {error && <Message msg="File not suported" />}
-
+      <Modal isOpen={modalIsOpen} style={customStyles} contentLabel="Picture">
+        <PictureForm
+          imgPreview={imgPreview}
+          ImageSubmit={ImageSubmit}
+          setImgPreview={setImgPreview}
+          HandleImagefile={HandleImagefile}
+          setIsOpen={setIsOpen}
+        />
+      </Modal>
+      {!imgPreview && (
+        <div className="profile-image-container">
+          <img src={`${URL.USERS_DB}/${idcli}/image`} />
+          <span className="edit center" onClick={() => setIsOpen(true)}>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M3.96867 13.9157L13.6592 4.41662C14.906 3.19446 16.9283 3.19446 18.175 4.41662L19.4694 5.6854C20.7162 6.90755 20.7162 8.88985 19.4694 10.112L9.74061 19.6486C9.1843 20.1939 8.43007 20.4999 7.64282 20.4999H3.65854C3.28841 20.4999 2.99098 20.201 3.00021 19.8383L3.10043 15.8975C3.12036 15.1526 3.43127 14.4425 3.96867 13.9157ZM18.5381 6.59831L17.2437 5.32953C16.5113 4.61156 15.323 4.61156 14.5905 5.32953L13.8382 6.06697L17.7862 9.93612L18.5381 9.19909C19.2705 8.48112 19.2705 7.31628 18.5381 6.59831ZM4.89998 14.8287L12.9069 6.97989L16.8549 10.849L8.8093 18.7357L8.70228 18.8317C8.4067 19.0745 8.03222 19.2088 7.64282 19.2088L4.33345 19.2084L4.41707 15.9305C4.42814 15.5169 4.60126 15.1215 4.89998 14.8287ZM21 19.8545C21 19.498 20.7052 19.2089 20.3415 19.2089H13.471L13.3816 19.2148C13.0602 19.2576 12.8125 19.5277 12.8125 19.8545C12.8125 20.211 13.1073 20.5 13.471 20.5H20.3415L20.4308 20.4941C20.7523 20.4514 21 20.1813 21 19.8545Z"
+                fill="#200E32"
+              />
+            </svg>
+          </span>
+        </div>
+      )}
       <div className="profile-summary">
+        {error &&<Message msg={"Imagen no soportada"} bgColor="#dc3545" />}
         <ul className="profile-summary-list">
           <li className="profile-summary-item">
             <span className="summary-value">20</span>
@@ -125,39 +124,3 @@ const HeaderProfile = () => {
 };
 
 export default HeaderProfile;
-
-// console.log(form.profilePicture);
-// if (form.profilePicture) {
-//   //guardando imagenes
-//   const formdata = new FormData();
-//   formdata.append("file", form.profilePicture);
-//   let requestOptions = {
-//     body: formdata,
-//     method: "POST",
-//   };
-//   fetch(`${URL.USERS_DB}/${idcli}/image`, requestOptions)
-//     .then((resp) => resp)
-//     .then((resp) => {
-//       console.log(resp);
-//       setResponse(true);
-//       setTimeout(() => setResponse(false), 5000);
-//     })
-//     .catch((error) =>
-//       console.log("ERROR NO REGISTRO LA IMAGEN", error)
-//     );
-//   return;
-// } else {
-
-// }
-
-{
-  /* <div className="form-group">
-<label htmlFor="file" className="file-content">
-  <i className="fas fa-upload file-icon"></i> Subir una imagen...
-  <input type="file" name="file0" id="file" onChange={fileChange} />
-</label>
-</div> */
-}
-{
-  /* <img src={`${URL.USERS_DB}/${idcli}/image`} /> */
-}
